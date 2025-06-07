82ca4a35fb8956d336e2e6601b06350ab3c08b26
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-06-07 13:31:13 -0300

    cxskjvncjxbvkjcxzbvjbxcvmbcxjv

--- app/components/mapview/mapView.tsx
+++ app/components/mapview/mapView.tsx
@@ -29,7 +29,6 @@ function MapUpdater({ lat, lng }: { lat: number; lng: number }) {
 
 export default function MapView({ position, abrigos }: MapViewProps) {
   useEffect(() => {
-    delete (L.Icon.Default.prototype as any)._getIconUrl;
     L.Icon.Default.mergeOptions({
       iconRetinaUrl:
         'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
